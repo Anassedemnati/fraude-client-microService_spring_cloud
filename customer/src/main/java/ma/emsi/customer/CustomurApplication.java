988cb33039ef78b37c3ac88d0eb46da5fd56d0d6
Anasse Demnati <demnati_anasse@emsi-edu.ma>
2022-04-25 06:10:12 +0200

    add first module customer micro_service

---
+++ customer/src/main/java/ma/emsi/customer/CustomurApplication.java
@@ -0,0 +1,11 @@
+package ma.emsi.customer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomurApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CustomurApplication.class,args);
+    }
+}
