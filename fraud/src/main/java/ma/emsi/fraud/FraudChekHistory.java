6dc64476bcc0a6a7317d4bbad9f448e94f869cca
Anasse DEMNATI <demnati_anasse@emsi-edu.ma>
2022-06-11 20:22:53 +0100

    add FarudCheckHistoryRepository

---
+++ fraud/src/main/java/ma/emsi/fraud/FraudChekHistory.java
@@ -0,0 +1,31 @@
+package ma.emsi.fraud;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class FraudChekHistory {
+    @Id
+    @SequenceGenerator(
+            name = "fraud_id_sequence",
+            sequenceName = "fraud_id_sequence"
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "fraud_id_sequence"
+    )
+    private Integer id;
+    private Integer CustomerId;
+    private Boolean isFroudster;
+    private LocalDateTime cratedAt;
+}
