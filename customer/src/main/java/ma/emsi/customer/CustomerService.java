05c09a50bb29d06d532365b03ccbb4ed55b26102
Anasse DEMNATI <demnati_anasse@emsi-edu.ma>
2022-06-11 21:45:51 +0100

    test fraudChekResponse

--- customer/src/main/java/ma/emsi/customer/CustomerService.java
+++ customer/src/main/java/ma/emsi/customer/CustomerService.java
@@ -3,11 +3,13 @@ package ma.emsi.customer;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 @AllArgsConstructor
 public class CustomerService{
     private final CustomerRepository customerRepository;
+    private final RestTemplate restTemplate;
     public void registerCustomer(CustomerRegistrationRequest request) {
     Customer customer = Customer.builder()
             .firstName(request.firstName())
@@ -16,9 +18,19 @@ public class CustomerService{
             .build();
     //TODO : check if email is valid
     //TODO : check if email not taken
+        customerRepository.saveAndFlush(customer);
         // TODO : check if fraudster
-    //store customer to db
-        customerRepository.save(customer);
+        FraudChekResponse fraudChekResponse = restTemplate.getForObject(
+                "http://localhost:8082/api/v1/fraud-check/{customerId}",
+                FraudChekResponse.class,
+                customer.getId()
+
+        );
+        if (fraudChekResponse.isFraudster()){
+            throw new IllegalStateException("fraudster");
+        }
+//        //store customer to db
+//        customerRepository.save(customer);
         // TODO : send notification
     }
 }
---
+++ customer/src/main/java/ma/emsi/customer/FraudChekResponse.java
@@ -0,0 +1,4 @@
+package ma.emsi.customer;
+
+public record FraudChekResponse(Boolean isFraudster) {
+}
