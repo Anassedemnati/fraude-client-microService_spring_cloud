4e3e9a1b6d7be0d2bebbd04c8c2fc8d776972e81
Anasse DEMNATI <demnati_anasse@emsi-edu.ma>
2022-06-11 21:56:11 +0100

    add log to FarudController

--- fraud/src/main/java/ma/emsi/fraud/FarudController.java
+++ fraud/src/main/java/ma/emsi/fraud/FarudController.java
@@ -2,16 +2,19 @@ package ma.emsi.fraud;
 
 
 import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/fraud-check")
 @Data
+@Slf4j
 public class FarudController {
     private final FarudCheckService farudCheckService;
     @GetMapping(path = "{customerId}")
     public FraudChekResponse isFraudster(@PathVariable("customerId") Integer customerId){
         boolean isfraudulentCustomer = farudCheckService.inFraudulentCustomer(customerId);
+        log.info("fraud check request for customer {}",customerId);
         return new FraudChekResponse(isfraudulentCustomer);
     }
 }
