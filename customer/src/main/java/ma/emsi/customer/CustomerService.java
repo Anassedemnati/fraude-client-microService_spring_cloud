581a56120ed06256d399686cee4feb05cd0b0213
Anasse DEMNATI <demnati_anasse@emsi-edu.ma>
2022-06-11 23:21:38 +0100

    add eureka comunication between services and adding loadBalancer

--- customer/src/main/java/ma/emsi/customer/CustomerService.java
+++ customer/src/main/java/ma/emsi/customer/CustomerService.java
@@ -21,7 +21,7 @@ public class CustomerService{
         customerRepository.saveAndFlush(customer);
         // TODO : check if fraudster
         FraudChekResponse fraudChekResponse = restTemplate.getForObject(
-                "http://localhost:8082/api/v1/fraud-check/{customerId}",
+                "http://FRAUD/api/v1/fraud-check/{customerId}",
                 FraudChekResponse.class,
                 customer.getId()
 
