216657c876686d51258a4ecf5571c92625e2956f
DEMNATI Anasse <demnati_anasse@emsi-edu.ma>
2022-08-10 21:41:34 +0100

    AMQPTemplate and JacksonConverter  SimpleRabbitListenerContainerFactory

--- amqp/src/main/java/ma/emsi/amqp/RabbitMQConfig.java
+++ amqp/src/main/java/ma/emsi/amqp/RabbitMQConfig.java
@@ -4,6 +4,7 @@ package ma.emsi.amqp;
 
 import lombok.AllArgsConstructor;
 import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
@@ -17,6 +18,12 @@ import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 public class RabbitMQConfig {
     private final ConnectionFactory connectionFactory;
 
+    /**
+     * this method allow as send message to the quee
+     * @return AmqpTemplate
+     */
+    //
+    @Bean
     public AmqpTemplate amqpTemplate(){
         RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
 
@@ -25,6 +32,17 @@ public class RabbitMQConfig {
         return rabbitTemplate;
     }
 
+    /**
+     * this methode allow as to recive message from quee using JacksonConverter()
+     * @return SimpleRabbitListenerContainerFactory
+     */
+    @Bean
+    public SimpleRabbitListenerContainerFactory simpleRabbitListenerContainerFactory(){
+       SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+       factory.setConnectionFactory(connectionFactory);
+       factory.setMessageConverter(JacksonConverter());
+       return factory;
+    }
     @Bean
     public MessageConverter JacksonConverter(){
 
