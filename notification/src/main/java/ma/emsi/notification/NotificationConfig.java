fec9c4cd30d99d587fd473c7870540850087b81d
DEMNATI Anasse <demnati_anasse@emsi-edu.ma>
2022-08-12 20:36:43 +0100

    Queue Topic and Binding

--- notification/src/main/java/ma/emsi/notification/NotificationConfig.java
+++ notification/src/main/java/ma/emsi/notification/NotificationConfig.java
@@ -3,7 +3,12 @@ package ma.emsi.notification;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -19,6 +24,22 @@ public class NotificationConfig {
     @Value("${rabbitmq.routing-keys.internal-notification}")
     private String internalNotificationRoutingKey;
 
+    @Bean
+    public TopicExchange internalTopicExchange() {
+        return new TopicExchange(this.internalExchange);
+    }
+    @Bean
+    public Queue notificationQueue(){
+        return new Queue(this.notificationQueues);
+    }
+    @Bean
+     public Binding internalToNotificationBinding(){
+        return BindingBuilder
+                .bind(notificationQueue())
+                .to(internalTopicExchange())
+                .with(this.internalNotificationRoutingKey);
+
+     }
 
 
 }
