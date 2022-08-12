83a84fce5ed55d49e55b1edb04f89bef18716cd9
DEMNATI Anasse <demnati_anasse@emsi-edu.ma>
2022-08-12 21:13:03 +0100

    Publishing Messages in NotificationApplication

--- notification/src/main/java/ma/emsi/notification/NotificationApplication.java
+++ notification/src/main/java/ma/emsi/notification/NotificationApplication.java
@@ -1,13 +1,21 @@
 package ma.emsi.notification;
 
+import ma.emsi.amqp.RabbitMQMessageProducer;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
 
 @EnableEurekaClient
-@SpringBootApplication
+@SpringBootApplication(
+        scanBasePackages = {
+                "ma.emsi.notification",
+                "ma.emsi.amqp",
+        }
+)
 @EnableFeignClients(
         basePackages = "ma.emsi.clients"
 )
@@ -17,4 +25,17 @@ public class NotificationApplication {
     public static void main(String[] args) {
         SpringApplication.run(NotificationApplication.class,args);
     }
+//    @Bean
+//    CommandLineRunner commandLineRunner(RabbitMQMessageProducer producer,
+//                                        NotificationConfig notificationConfig){
+//        return args -> {
+//            producer.publish(
+//                    new Person("Anasse",22),
+//                    notificationConfig.getInternalExchange(),
+//                    notificationConfig.getInternalNotificationRoutingKey()
+//            );
+//
+//        };
+//    }
+//    record Person(String name,int age){}
 }
